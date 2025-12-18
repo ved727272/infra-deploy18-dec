9b4b1848ded957be3334e575f1bb0bd35f4590c3
ved727272 <brijesh.zte@gmail.com>
2025-12-19 00:43:24 +0530

    first commit

--- modules/azurem_load_balancer/variable.tf
+++
@@ -1,8 +0,0 @@
-variable "lb_name" {}
-  variable "location" {}
-  variable "resource_group_name" {}
-    variable "frontend_ip_configuration_name" {}
-    variable "backendpool_name" {}
-    variable "probe_name" {}
-    variable "lb_rule_name" {}
-    variable "pip_name" {}
--- modules/azurerm_nic_lb_association/variable.tf
+++
@@ -1,5 +0,0 @@
-variable "nic_name" {}
-  variable "lb_name" {}   
-  variable "lb_pool" {}   
-  variable "resource_group_name" {} 
-    variable "ip_configuration_name" {}
