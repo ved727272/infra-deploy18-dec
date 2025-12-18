44294ab5d30f89166a6fa794a54e34e76fa10d20
ved727272 <brijesh.zte@gmail.com>
2025-12-19 00:39:18 +0530

    Update resource group and resource names to '18'

--- environment/main.tf
+++ environment/main.tf
@@ -1,24 +1,24 @@
 module "rg" {
   source = "../modules/azurerm_resource_group"
-  resource_group_name = "rg_brijesh_28"
+  resource_group_name = "rg_brijesh_18"
   location = "westus"
   
 }
 module "vnet" {
   depends_on = [ module.rg ]
   source = "../modules/azurerm_virtual_network"
-  resource_group_name = "rg_brijesh_28"
+  resource_group_name = "rg_brijesh_18"
   location = "westus"
-  vnet_name = "vnet_brijesh_28"
+  vnet_name = "vnet_brijesh_18"
   address_space = ["10.0.0.0/16"]
 }
 
 module "subnet" {
   depends_on = [ module.vnet ]
   source = "../modules/azurerm_subnet"
-  resource_group_name = "rg_brijesh_28"
-  vnet_name = "vnet_brijesh_28"
-  subnet_name = "subnet_brijesh_28"
+  resource_group_name = "rg_brijesh_18"
+  vnet_name = "vnet_brijesh_18"
+  subnet_name = "subnet_brijesh_18"
   address_prefixes = ["10.0.1.0/24"]
   
 }
@@ -26,8 +26,8 @@ module "subnet" {
 module "pip" {
   depends_on = [ module.rg ]
   source = "../modules/azurerm_public_ip"
-  resource_group_name = "rg_brijesh_28"
+  resource_group_name = "rg_brijesh_18"
   location = "westus"
-  pip_name = "pip_brijesh_28"
+  pip_name = "pip_brijesh_18"
 }
 
